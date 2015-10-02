ec4bcbff5dfd4fa1be1f0d81fde4c0cd3c85010b
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-10-02 17:17:58 +0200

    Create harbour-sailfish-dict-sv.ts

---
+++ translations/harbour-sailfish-dict-sv.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Translate clipboard</source>
+        <translation>Översätt urklipp</translation>
+    </message>
+    <message>
+        <source>Translate</source>
+        <translation>Översätt</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Search a dictionary</source>
+        <translation>Sök i en ordbok</translation>
+    </message>
+    <message>
+        <source>Search for</source>
+        <translation>Sök efter</translation>
+    </message>
+    <message>
+        <source>Search for a word</source>
+        <translation>Sök efter ett ord</translation>
+    </message>
+    <message>
+        <source>No translations found!</source>
+        <translation>Ingen översättning hittades!</translation>
+    </message>
+    <message>
+        <source>Seems there is an error we didn&apos;t know about. Please report it.</source>
+        <translation>Det verkar finnas ettokänt  fel. Var snäll och rapportera det.</translation>
+    </message>
+    <message>
+        <source>You need a internet connection to search.</source>
+        <translation>Du måste ha internetanslutning för att söka.</translation>
+    </message>
+    <message>
+        <source>Error on requesting dict data on freedict.org/dictd for &quot;%1&quot;</source>
+        <translation>Fel vid begäran om orboksdata från freedict.org/dictd, för &quot;%1&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Translations</source>
+        <translation>Översättningar</translation>
+    </message>
+</context>
+</TS>
